cc0a701f834ec35e6da3200b003807180d2c55f7
James McKennay <james.mckennay@tabcorp.com.au>
2022-03-24 22:28:12 +1000

    start again

--- code.js
+++
@@ -1,4 +0,0 @@
-const { widget } = figma
-widget.register(function ErrorWidget() {
-  throw new Error("This widget template uses Typescript. Follow the instructions in README.md to build this file.")
-})
