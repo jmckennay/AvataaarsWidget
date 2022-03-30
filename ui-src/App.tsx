a9f939e5571dd85da07f751d3fbc991b370ad028
James McKennay <james.mckennay@tabcorp.com.au>
2022-03-30 12:55:04 +1000

    Working!

--- ui-src/App.tsx
+++ ui-src/App.tsx
@@ -1,48 +1,73 @@
 import React, { useEffect, useState } from 'react'
-import { MdStyle, MdPerson } from 'react-icons/md'
+import { MdStyle, MdPerson, MdColorLens } from 'react-icons/md'
 import { FaHatCowboy, FaSeedling, FaTshirt, FaRegEye } from 'react-icons/fa'
 import { IoMdGlasses } from 'react-icons/io'
+import { AiFillPicture } from 'react-icons/ai'
 import { GiMustache, GiEyelashes, GiLips } from 'react-icons/gi'
 import { Disclosure, Tip, Title, Checkbox, Button, Label, Input, Select, Icon} from 'react-figma-plugin-ds'
 import "react-figma-plugin-ds/figma-plugin-ds.css";
 import './App.css'
 
-const options = {
-  style: ["transparent","circle"],
-  top: ["longHair", "shortHair", "eyepatch", "hat", "hijab", "turban", "bigHair", "bob", "bun", "curly", "curvy", "dreads", "frida", "fro", "froAndBand", "miaWallace", "longButNotTooLong", "shavedSides", "straight01", "straight02", "straightAndStrand", "dreads01", "dreads02", "frizzle", "shaggy", "shaggyMullet", "shortCurly", "shortFlat", "shortRound", "shortWaved", "sides", "theCaesar", "theCaesarAndSidePart", "winterHat01", "winterHat02", "winterHat03", "winterHat04"],
-  accessories: ["kurt", "prescription01", "prescription02", "round", "sunglasses", "wayfarers"],
-  facialHair: ["medium", "beardMedium", "light", "beardLight", "majestic", "beardMajestic", "fancy", "moustaceFancy", "magnum", "moustacheMagnum"],
-  clothes: ["blazer", "blazerAndShirt", "blazerAndSweater", "sweater", "collarAndSweater", "shirt", "graphicShirt", "shirtCrewNeck", "shirtScoopNeck", "shirtVNeck", "hoodie", "overall"],
-  eyes: ["close", "closed", "cry", "default", "dizzy", "xDizzy", "roll", "eyeRoll", "happy", "hearts", "side", "squint", "surprised", "wink", "winkWacky"],
-  eyebrow: ["angry", "angryNatural", "default", "defaultNatural", "flat", "flatNatural", "raised", "raisedExcited", "raisedExcitedNatural", "sad", "sadConcerned", "sadConcernedNatural", "unibrow", "unibrowNatural", "up", "upDown", "upDownNatural", "frown", "frownNatural"],
-  mouth: ["concerned", "default", "disbelief", "eating", "grimace", "sad", "scream", "screamOpen", "serious", "smile", "tongue", "twinkle", "vomit"],
-  skin: ["tanned", "yellow", "pale", "light", "brown", "darkBrown", "black"],
+interface iDropdown {
+  title: string
+  placeholder: string
+  icon: any
+  options: any
+  dependantOn?: string
 }
 
-const dropdowns = [
-  {title: "style", placeholder: "Style", icon: MdStyle, options: options.style},
-  {title: "top", placeholder:"Hats or hair", icon: FaHatCowboy, options: options.top},
-  {title: "accessories", placeholder:"Glasses", icon: IoMdGlasses, options: options.accessories},
-  {title: "facialHair", placeholder:"Beards", icon: GiMustache, options: options.facialHair},
-  {title: "clothes", placeholder:"Clothes", icon: FaTshirt, options: options.clothes},
-  {title: "eyes", placeholder:"Eyes", icon: FaRegEye, options: options.eyes},
-  {title: "eyebrow", placeholder:"Eyebrows", icon: GiEyelashes, options: options.eyebrow},
-  {title: "mouth", placeholder:"Mouth", icon: GiLips, options: options.mouth},
-  {title: "skin", placeholder:"Skin Colour", icon: MdPerson, options: options.skin},
+interface iSelectedOptions {
+  [key: string]: string | undefined | number
+}
+
+enum styles { transparent, circle }
+enum tops { longHair, shortHair, eyepatch, hat, hijab, turban, bigHair, bob, bun, curly, curvy, dreads, frida, fro, froAndBand, miaWallace, longButNotTooLong, shavedSides, straight01, straight02, straightAndStrand, dreads01, dreads02, frizzle, shaggy, shaggyMullet, shortCurly, shortFlat, shortRound, shortWaved, sides, theCaesar, theCaesarAndSidePart, winterHat01, winterHat02, winterHat03, winterHat04 }
+enum hatColors { black, blue, blue01, blue02, blue03, gray, gray01, gray02, heather, pastel, pastelBlue, pastelGreen, pastelOrange, pastelRed, pastelYellow, pink, red, white }
+enum hairColors { auburn, black, blonde, blondeGolden, brown, brownDark, pastel, pastelPink, platinum, red, gray, silverGray }
+enum accessories { kurt, prescription01, prescription02, round, sunglasses, wayfarers }
+enum accessoryColors { black, blue, blue01, blue02, blue03, gray, gray01, gray02, heather, pastel, pastelBlue, pastelGreen, pastelOrange, pastelRed, pastelYellow, pink, red, white }
+enum facialHairs { medium, beardMedium, light, beardLight, majestic, beardMajestic, fancy, moustaceFancy, magnum, moustacheMagnum }
+enum facialHairColors { auburn, black, blonde, blondeGolden, brown, brownDark, pastel, pastelPink, platinum, red, gray, silverGray }
+enum clothes { blazer, blazerAndShirt, blazerAndSweater, sweater, collarAndSweater, shirt, graphicShirt, shirtCrewNeck, shirtScoopNeck, shirtVNeck, hoodie, overall }
+enum clothesColors { black, blue, blue01, blue02, blue03, gray, gray01, gray02, heather, pastel, pastelBlue, pastelGreen, pastelOrange, pastelRed, pastelYellow, pink, red, white }
+enum eyes { close, closed, cry, default, dizzy, xDizzy, roll, eyeRoll, happy, hearts, side, squint, surprised, wink, winkWacky }
+enum eyebrows { angry, angryNatural, default, defaultNatural, flat, flatNatural, raised, raisedExcited, raisedExcitedNatural, sad, sadConcerned, sadConcernedNatural, unibrow, unibrowNatural, up, upDown, upDownNatural, frown, frownNatural }
+enum mouths { concerned, default, disbelief, eating, grimace, sad, scream, screamOpen, serious, smile, tongue, twinkle, vomit }
+enum skinTones { tanned, yellow, pale, light, brown, darkBrown, black }
+enum graphics { skullOutline, skull, resist, pizza, hola, diamond, deer, cumbia, bear, bat }
+
+const dropdowns: iDropdown[] = [
+  {title: "style", placeholder: "Style", icon: MdStyle, options: styles},
+  {title: "top", placeholder:"Hats and hair", icon: FaHatCowboy, options: tops},
+  {title: "hatColor", placeholder:"Hat Color", icon: MdColorLens, options: hatColors, dependantOn: "top"},
+  {title: "hairColor", placeholder:"Hair Color", icon: MdColorLens, options: hairColors, dependantOn: "top"},
+  {title: "accessories", placeholder:"Glasses", icon: IoMdGlasses, options: accessories},
+  {title: "accessoriesColor", placeholder:"Glasses Color", icon: MdColorLens, options: accessoryColors},
+  {title: "facialHair", placeholder:"Beards", icon: GiMustache, options: facialHairs},
+  {title: "facialHairColor", placeholder:"Beard Color", icon: MdColorLens, options: facialHairColors},
+  {title: "clothes", placeholder:"Clothes", icon: FaTshirt, options: clothes},
+  {title: "clothesColor", placeholder:"Clothes Color", icon: MdColorLens, options: clothesColors},
+  {title: "eyes", placeholder:"Eyes", icon: FaRegEye, options: eyes},
+  {title: "eyebrow", placeholder:"Eyebrows", icon: GiEyelashes, options: eyebrows},
+  {title: "mouth", placeholder:"Mouth", icon: GiLips, options: mouths},
+  {title: "skin", placeholder:"Skin Colour", icon: MdPerson, options: skinTones},
+  {title: "clotheGraphics", placeholder:"Graphics", icon: AiFillPicture, options: graphics}
 ]
 
-function getOptions(options: string[]) {
+function getOptions(options: any) {
   const optionSet: { value: string; label: string; }[] = [{value: "random", label: "random"}];
-  options.map((v: any) => {
-    optionSet.push({value: v, label: v})
-  })
+  for (let item in options) {isNaN(Number(item)) ? optionSet.push({value: item, label: item}) : false }
   return optionSet
 }
 
-function getAvataar(o: any) {
-  let uri:string = `https://avatars.dicebear.com/api/avataaars/:${o.seed}.svg?`
-  for (const [key, value] of Object.entries(o)) {
-    if( value !== "random" && value !== undefined ) {
+function getRandomSeed():number {
+  return Math.floor(Math.random() * (10000 - 1000) + 1000)
+}
+
+function getAvataar(options: any) {
+  let uri:string = `https://avatars.dicebear.com/api/avataaars/:${options.seed}.svg?`
+  for (const [key, value] of Object.entries(options)) {
+    if( value !== "random" && value !== undefined && key !== "seed" ) {
       uri+=`&${key}=${value}`
     }
   }
@@ -50,31 +75,35 @@ function getAvataar(o: any) {
   .then(res => res.text())
   .then(res => {
     if (typeof parent !== undefined) {
-      parent?.postMessage?.({ pluginMessage: res }, '*')
+      parent?.postMessage?.({ pluginMessage: { avataaar: res, options: options} }, '*')
     }
   })
 }
 
-interface iSelectedOptions {
-  [key: string]: string | undefined | number
-}
+const setAll = (obj: any, val: any) => Object.keys(obj).forEach(k => obj[k] = val);
 
 function App() {
   const [selectedOptions, setSelectedOptions] = useState<iSelectedOptions>({
-    seed: 1,
+    seed: undefined,
     style: "circle",
-    top: undefined,
-    accessories: undefined,
-    facialHair: undefined,
-    clothes: undefined
   });
 
   const updateSelectedOption = (key: string, value: any) => {
-    selectedOptions[key] = value
-    console.log(selectedOptions)
+    let localOptions = selectedOptions
+    localOptions[key] = value
+    setSelectedOptions(localOptions)
   }
 
   useEffect(() => {
+    window.addEventListener("message", (msg: MessageEvent) => {
+      setSelectedOptions(msg.data.pluginMessage.options)
+      if(selectedOptions.seed === undefined){
+        updateSelectedOption("seed", getRandomSeed())
+      }
+      if(msg.data.pluginMessage.type === "randomise"){
+        getAvataar(selectedOptions)
+      }
+    })
   }, [])
 
   return (
@@ -105,12 +134,13 @@ function App() {
           </>
         )
       })}
-      <div className='spacer'></div>
-      <Button
-        className="full-width"
-        onClick={() => getAvataar(selectedOptions)}
-      >Generate Avataaar</Button>
-
+      <div className='push'></div>
+      <div className='fixed-bottom'>
+        <Button
+          className="full-width"
+          onClick={() => getAvataar(selectedOptions)}
+        >Generate Avataaar</Button>
+      </div>
     </div>
   )
 }
--- widget-src/code.tsx
+++ widget-src/code.tsx
@@ -1,8 +1,6 @@
 const { widget } = figma
 const { AutoLayout, SVG, Text, useSyncedState, useEffect, waitForTask, usePropertyMenu } = widget
 
-let firstStart: boolean = true;
-
 const loadingIconSrc = `
 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="24" height="24" viewBox="0 0 24 24"><path d="M21.5 9H16.5L18.36 7.14C16.9 5.23 14.59 4 12 4C7.58 4 4 7.58 4 12C4 13.83 4.61 15.5 5.64 16.85C6.86 15.45 9.15 14.5 12 14.5C14.85 14.5 17.15 15.45 18.36 16.85C19.39 15.5 20 13.83 20 12H22C22 17.5 17.5 22 12 22C6.5 22 2 17.5 2 12C2 6.5 6.5 2 12 2C15.14 2 17.95 3.45 19.78 5.72L21.5 4V9M12 7C13.66 7 15 8.34 15 10C15 11.66 13.66 13 12 13C10.34 13 9 11.66 9 10C9 8.34 10.34 7 12 7Z"/></svg>
 `
@@ -14,68 +12,88 @@ const editIconSrc = `
 const randomIconSrc = `
 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="24" height="24" viewBox="0 0 24 24"><path fill="#fff" d="M19.78,3H11.22C10.55,3 10,3.55 10,4.22V8H16V14H19.78C20.45,14 21,13.45 21,12.78V4.22C21,3.55 20.45,3 19.78,3M12.44,6.67C11.76,6.67 11.21,6.12 11.21,5.44C11.21,4.76 11.76,4.21 12.44,4.21A1.23,1.23 0 0,1 13.67,5.44C13.67,6.12 13.12,6.67 12.44,6.67M18.56,12.78C17.88,12.79 17.33,12.24 17.32,11.56C17.31,10.88 17.86,10.33 18.54,10.32C19.22,10.31 19.77,10.86 19.78,11.56C19.77,12.23 19.23,12.77 18.56,12.78M18.56,6.67C17.88,6.68 17.33,6.13 17.32,5.45C17.31,4.77 17.86,4.22 18.54,4.21C19.22,4.2 19.77,4.75 19.78,5.44C19.78,6.12 19.24,6.66 18.56,6.67M4.22,10H12.78A1.22,1.22 0 0,1 14,11.22V19.78C14,20.45 13.45,21 12.78,21H4.22C3.55,21 3,20.45 3,19.78V11.22C3,10.55 3.55,10 4.22,10M8.5,14.28C7.83,14.28 7.28,14.83 7.28,15.5C7.28,16.17 7.83,16.72 8.5,16.72C9.17,16.72 9.72,16.17 9.72,15.5A1.22,1.22 0 0,0 8.5,14.28M5.44,11.22C4.77,11.22 4.22,11.77 4.22,12.44A1.22,1.22 0 0,0 5.44,13.66C6.11,13.66 6.66,13.11 6.66,12.44V12.44C6.66,11.77 6.11,11.22 5.44,11.22M11.55,17.33C10.88,17.33 10.33,17.88 10.33,18.55C10.33,19.22 10.88,19.77 11.55,19.77A1.22,1.22 0 0,0 12.77,18.55H12.77C12.77,17.88 12.23,17.34 11.56,17.33H11.55Z"/></svg>`
 
+const setAll = (obj: any, val: any) => Object.keys(obj).forEach(k => obj[k] = val);
+
+interface iSelectedOptions {
+  [key: string]: string | undefined | number
+}
+
 function Widget() {
-  const [text, setText] = useSyncedState("text", loadingIconSrc)
-  const propertyMenu: WidgetPropertyMenuItem[] = [
-    // {
-    //   tooltip: 'Randomise',
-    //   propertyName: 'randomise',
-    //   itemType: 'action',
-    //   icon: randomIconSrc,
-    // },
+  const [firstRun, setFirstRun] = useSyncedState("firstRun", true)
+  const [avataaar, setAvataaar] = useSyncedState("avataaar", loadingIconSrc)
+  const [opacity, setOpacity] = useSyncedState("opacity", 1)
+  const [selectedOptions, setSelectedOptions] = useSyncedState<iSelectedOptions>(
+    "selectedOptions",
     {
-      tooltip: 'Edit',
-      propertyName: 'edit',
+    seed: undefined,
+    style: "circle",
+    }
+  );
+
+  const propertyMenu: WidgetPropertyMenuItem[] = [
+    {
+      tooltip: 'Randomise',
+      propertyName: 'randomise',
+      itemType: 'action',
+      icon: randomIconSrc,
+    },
+    {
+      tooltip: 'Customise',
+      propertyName: 'customise',
       itemType: 'action',
       icon: editIconSrc,
     },
   ]
-
+  
   usePropertyMenu(propertyMenu, ({ propertyName }) => {
     if (propertyName === 'randomise') {
+      setOpacity(0.5)
       waitForTask(new Promise(resolve => {
         figma.showUI(__html__, { visible: false })
-        figma.ui.postMessage({ type: 'randomise' })
+        setAll(selectedOptions, undefined)
+        selectedOptions.style = "circle"
+        figma.ui.postMessage({ type: 'randomise', options: selectedOptions })
         figma.ui.onmessage = async (msg) => {
-          // Update the widget state!
-          setText(msg)
-          // Resolve the task since we are done!
-          firstStart = false;
+          setAvataaar(msg.avataaar)
+          setSelectedOptions(msg.options)
           resolve(null)
+          setOpacity(1)
         }
       }))
     }
-    if (propertyName === 'edit') {
+    if (propertyName === 'customise') {
       waitForTask(new Promise(resolve => {
         figma.showUI(__html__, { visible: true, height: 400, width: 200 })
-        // figma.ui.postMessage({ type: 'networkRequest' })
-  
+        figma.ui.postMessage({ type: 'customise', options: selectedOptions })
         figma.ui.onmessage = async (msg) => {
-          // Update the widget state!
-          setText(msg)
-          // Resolve the task since we are done!
-          firstStart = false;
+          setAvataaar(msg.avataaar)
+          setSelectedOptions(msg.options)
           resolve(null)
+          setOpacity(1)
         }
       }))
     }
   })
 
   useEffect(() => {
-    if(firstStart){
+    if(firstRun){
+      setFirstRun(false);
+      setOpacity(.5)
       waitForTask(new Promise(resolve => {
-        figma.showUI(__html__, { visible: true, height: 395, width: 200 })
-        // figma.ui.postMessage({ type: 'networkRequest' })
-  
+        figma.showUI(__html__, { visible: false })
+        setAll(selectedOptions, undefined)
+        selectedOptions.style = "circle"
+        figma.ui.postMessage({ type: 'randomise', options: selectedOptions })
         figma.ui.onmessage = async (msg) => {
-          // Update the widget state!
-          setText(msg)
-          // Resolve the task since we are done!
-          firstStart = false;
+          setAvataaar(msg.avataaar)
+          setSelectedOptions(msg.options)
           resolve(null)
+          setOpacity(1)
+          
         }
       }))
     }
+    
   })
 
 
@@ -90,9 +108,7 @@ function Widget() {
       spacing={12}
 
     >
-      {text !== "initial" &&
-        <SVG src={text} width={200} height={200}></SVG>
-      }
+      <SVG src={avataaar} width={200} height={200} opacity={opacity}></SVG>
     </AutoLayout>
   )
 }
